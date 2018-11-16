581533e9294452ce5f74779df1b223c6acb3f768
u1859269 <U1859269@unimail.hud.ac.uk>
2018-11-16 13:57:09 +0000

    Default Changelist

--- Week 6 - Practical 5/Binary loop.py
+++ Week 6 - Practical 5/Binary loop.py
@@ -1,9 +1,16 @@
 """
+
+Binary Number Loop
+
+This program loops through 128 denary (Base 10) numbers and converts them to 8 bit binary (Base 2)
+numbers by using a loop
+
 """
 """
 __Author__ = Emaz Khan
 __Email__ = U1859269@unimail.hud.ac.uk
 __Date__ = Thursday 8th November 2018
 """
-for i in range(0 , 128):
-    print(i)
+
+for i in range(128):
+    print(str(i) + " " + bin(i)[2:].zfill(8))
